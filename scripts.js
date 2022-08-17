037ed44e4254f94acb70d6da528aac1a4933055a
Pedro Bezerra <phdesign9@gmail.com>
2022-08-17 15:17:09 -0300

    inputs and game screen

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
-
 let playerScore = 0;
-let ComputerScore = 0;
-
+let computerScore = 0;
+let result = '';
 
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
@@ -14,34 +13,73 @@ btn.addEventListener('click', () => {
 
 
 
-function getComputerChoice(){
-  let choices = [
-    'rock', 'paper', 'scissors'
-  ]
-   let randomChoice = Math.floor(Math.random() * choices.length);
-  if (randomChoice === 0) {
-    randomChoice = choices[0];
-  }
+const choices = {
+  '#rockBtn': 'rock',
+  '#paperBtn': 'paper',
+  '#scissorsBtn': 'scissors'
+}
+playerChoice = "";
+let pcChoice = 0;
 
-  if (randomChoice === 1) {
-    randomChoice = choices[1];
-  }
+for (let choice in choices) {
+  const theChoice = choices[choice];
+  const btnPlayerChoice = document.querySelector(choice);
+  btnPlayerChoice.addEventListener("click", () => {
+    playerChoice = theChoice;
+    
+    pcChoice = Math.floor(Math.random() * choices[choice].length);
+    if (pcChoice === 1) {
+      computerChoice = 'rock';
+    }
+    if (pcChoice === 2) {
+      computerChoice = 'paper';
+    }
+    if (pcChoice === 3) {
+      computerChoice = 'scissors';
+    }
+    console.log(playerChoice);
+    console.log(computerChoice);
 
-  if (randomChoice === 2) {
-    randomChoice = choices[2];
-  }
- return randomChoice;
-  
+    if(
+      (playerChoice == 'rock' && computerChoice == 'scissors')||
+      (playerChoice == 'paper' && computerChoice == 'rock')||
+      (playerChoice == 'scissors' && computerChoice == 'paper')){
+        result='Humam win';
+        playerScore++;
+        let placar = `O placar é : player: ${playerScore} x Computer: ${computerScore}`
+        console.log(result);
+        console.log(placar);
+    }
+
+    if(
+      (computerChoice == 'rock' && playerChoice == 'scissors')||
+      (computerChoice == 'paper' && playerChoice == 'rock')||
+      (computerChoice == 'scissors' && playerChoice == 'paper')){
+        result='Computer win';
+        computerScore++;
+        let placar = `O placar é : player: ${playerScore} x Computer: ${computerScore}`
+        console.log(result);
+        console.log(placar);
+        
+    }
+
+    if(playerChoice === computerChoice){
+      result='empate';
+      console.log(result);
+    }
+    
+  })
+
 }
 
+/*
 function getPlayerChoice() {
-
+  
+  playerChoice = "";
   const rockBtn = document.querySelector('#rockBtn');
   rockBtn.addEventListener('click', () => {
-    let playerChoice = 'rock';
-    console.log(playerChoice);
-    
-
+   
+     
   });
 
   const paperBtn = document.querySelector('#paperBtn')
@@ -61,11 +99,14 @@ function getPlayerChoice() {
       
   });
 
-  
+  if(pcChoice === playerChoice){
+    console.log("empate")
+   }
   
 }
-
-
+getPlayerChoice()
+*/
+/*
 function game( ){
 
   getComputerChoice()
@@ -93,18 +134,7 @@ function game( ){
 }
 game();
 
-function playRound(){
-  if( playerScore <3 || ComputerScore <3 ){
-    game();
-    alert('ainda tem jogo');
-  }
-  if ( playerScore ===3 ){
-    console.log('the Player win this game')
-  }
-  if ( ComputerScore ===3 ){
-    console.log('the thecomputer win this game')
-  }
-}
+*/
 
 
 
