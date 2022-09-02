b8637eb2ec5e525e7a30f5f874792b16a850d815
Pedro Bezerra <phdesign9@gmail.com>
2022-09-02 04:53:52 -0300

    gameCompleteREF

--- scripts.js
+++ scripts.js
@@ -1,24 +1,70 @@
-let playerScore = 0;
-let computerScore = 0;
-let gameID = 0;
-let winnerName = "";
-let loserName = "";
-let winnerScore = 0;
-let loserScore = 0;
 const screenOne = document.querySelector(".startScreen");
-const screenTwo = document.querySelector(".gameScreen");
-const screenThree = document.querySelector(".finalScreen");
-
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', () => {
+const btnStart = document.querySelector('#btn');
+btnStart.addEventListener('click', () => {
   screenOne.style.display = "none";
   screenTwo.style.display = "flex";
-
 });
 
+const screenTwo = document.querySelector(".gameScreen");
+const screenThree = document.querySelector(".finalScreen");
 function endGame() {
   screenThree.style.display = "flex";
   screenTwo.style.display = "none";
+}
+
+const painelPlayer = document.querySelector("#playerChoiceCard");
+const painelComputer = document.querySelector("#machineChoiceCard");
+function renderChoices() {
+  painelPlayer.src = `images/${playerChoice}.png`;
+  painelComputer.src = `images/${computerChoice}.png`;
+}
+
+let gameID = 0;
+const winner = document.querySelector("#winnerGame");
+const resultGame = document.querySelector('.resultScore');
+function recordGame(winnerName, loserName, winnerScore, loserScore) {
+  gameID++;
+  winner.innerText = winnerName;
+  const gameLog = document.createElement('p');
+  gameLog.classList.add('resultScore__text');
+  gameLog.innerText = `Game ${gameID} Winner - ${winnerName}: ${winnerScore} x ${loserScore} ${loserName}`;
+  resultGame.appendChild(gameLog);
+  endGame();
+}
+
+let playerScore = 0;
+let computerScore = 0;
+const playerCounter = document.querySelector("#playerScorePainel");
+const computerCounter = document.querySelector("#computerScorePainel");
+const roundWinner = document.querySelector("#resultPhasePainel");
+function gameRules() {
+  if (
+    (playerChoice == 'rock' && computerChoice == 'scissors') ||
+    (playerChoice == 'paper' && computerChoice == 'rock') ||
+    (playerChoice == 'scissors' && computerChoice == 'paper')) {
+    roundWinner.innerText = 'You win this round';
+    playerScore++;
+    playerCounter.innerText = playerScore;
+  }
+  else if (
+    (computerChoice == 'rock' && playerChoice == 'scissors') ||
+    (computerChoice == 'paper' && playerChoice == 'rock') ||
+    (computerChoice == 'scissors' && playerChoice == 'paper')) {
+    roundWinner.innerText = 'Machine win this round';
+    computerScore++;
+    computerCounter.innerText = computerScore;
+  }
+
+  if (playerChoice === computerChoice) {
+    roundWinner.innerText = 'Draw';
+  }
+
+  if (computerScore == 5) {
+    recordGame('Machine', 'You', computerScore, playerScore);
+  }
+  else if (playerScore == 5) {
+    recordGame('You', 'Machine', playerScore, computerScore);
+  }
 }
 
 const choices = {
@@ -26,95 +72,33 @@ const choices = {
   '#paperBtn': 'paper',
   '#scissorsBtn': 'scissors'
 }
-let playerChoice = "";
-let randomChoice = 0;
 
-const painelPlayer = document.querySelector("#playerChoiceCard");
-const painelComputer = document.querySelector("#machineChoiceCard");
-const playerCounter = document.querySelector("#playerScorePainel");
-const computerCounter = document.querySelector("#computerScorePainel");
-const roundWinner = document.querySelector("#resultPhasePainel");
-const winner = document.querySelector("#winnerGame");
-const resultGame = document.querySelector('.resultScore');
-function recordGame() {
-  const gameLog = document.createElement('p');
-  gameLog.classList.add('resultScore__text');
-  gameLog.innerText = `Game ${gameID} Winner - ${winnerName}: ${winnerScore} x ${loserScore} ${loserName}`;
-  resultGame.appendChild(gameLog);
+function getcomputerChoice() {
+  randomChoice = Math.floor(Math.random() * 3);
+  if (randomChoice === 0) {
+    return 'rock';
+  }
+  if (randomChoice === 1) {
+    return 'paper';
+  }
+  if (randomChoice === 2) {
+    return 'scissors';
+  }
 }
 
 for (let choice in choices) {
   const theChoice = choices[choice];
   const btnPlayerChoice = document.querySelector(choice);
-
   btnPlayerChoice.addEventListener("click", () => {
     playerChoice = theChoice;
-
-    randomChoice = Math.floor(Math.random() * choices[choice].length);
-    if (randomChoice === 1) {
-      computerChoice = 'rock';
-    }
-    if (randomChoice === 2) {
-      computerChoice = 'paper';
-    }
-    if (randomChoice === 3) {
-      computerChoice = 'scissors';
-    }
-
-    painelPlayer.src = `images/${playerChoice}.png`;
-    painelComputer.src = `images/${computerChoice}.png`;
-    if (
-      (playerChoice == 'rock' && computerChoice == 'scissors') ||
-      (playerChoice == 'paper' && computerChoice == 'rock') ||
-      (playerChoice == 'scissors' && computerChoice == 'paper')) {
-      roundWinner.innerText = 'You win this round';
-      playerScore++;
-      playerCounter.innerText = playerScore;
-
-
-    }
-
-    if (
-      (computerChoice == 'rock' && playerChoice == 'scissors') ||
-      (computerChoice == 'paper' && playerChoice == 'rock') ||
-      (computerChoice == 'scissors' && playerChoice == 'paper')) {
-      roundWinner.innerText = 'Machine win this round';
-      computerScore++;
-      computerCounter.innerText = computerScore;
-    }
-
-    if (playerChoice === computerChoice) {
-      roundWinner.innerText = 'Draw';
-    }
-
-    if (computerScore == 3) {
-      gameID++;
-      winnerName = 'Machine';
-      loserName = "You";
-      winnerScore = computerScore;
-      loserScore = playerScore;
-      winner.innerText = winnerName;
-      endGame();
-      recordGame();
-    }
-
-    if (playerScore == 3) {
-      gameID++;
-      winnerName = 'You';
-      loserName = 'Machine';
-      winnerScore = playerScore;
-      loserScore = computerScore;
-      winner.innerText = winnerName;
-      endGame();
-      recordGame();
-    }
-
-  })
+    computerChoice = getcomputerChoice();
+    renderChoices();
+    gameRules();
+  });
 
 }
 
-const restartGameBtn = document.querySelector("#restartGame");
-restartGameBtn.addEventListener('click', () => {
+function restartGame() {
   screenThree.style.display = "none";
   screenTwo.style.display = "flex";
   playerScore = 0;
@@ -126,4 +110,9 @@ restartGameBtn.addEventListener('click', () => {
   painelPlayer.src = "";
   painelComputer.src = "";
   roundWinner.innerText = "";
+}
+
+const restartGameBtn = document.querySelector("#restartGame");
+restartGameBtn.addEventListener('click', () => {
+  restartGame();
 });
